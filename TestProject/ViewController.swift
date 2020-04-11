d0b5af660b899335df024077291fc1672ab119b7
Dennis <nndenninn@gmail.com>
2020-04-11 18:34:58 +0300

    Very test simle app

--- TestProject/ViewController.swift
+++ TestProject/ViewController.swift
@@ -9,12 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        label.text = "Select an Item"
     }
-
-
+    
+    @IBAction func tappedButton(_ sender: UIButton) {
+        switch sender.tag {
+        case 0:
+            label.text = "Rose"
+        case 1:
+            label.text = "Lemons"
+        case 2:
+            label.text = "Apples"
+        case 3:
+            label.text = "Eggs"
+        default:
+            return
+        }
+    }
 }
 
